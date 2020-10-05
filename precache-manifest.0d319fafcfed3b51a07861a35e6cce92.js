ff2c274bb57e91a7eaa8445e9dd986a269987c7e
Sergey <shapa92sergey@gmail.com>
2020-10-06 01:56:18 +0300

    Updates

---
+++ precache-manifest.0d319fafcfed3b51a07861a35e6cce92.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c66c45fd3f861fdfcecd85316abae006",
+    "url": "/redux-toolkit-12/index.html"
+  },
+  {
+    "revision": "cbbc088eaf32b3be5f85",
+    "url": "/redux-toolkit-12/static/css/main.595014ad.chunk.css"
+  },
+  {
+    "revision": "e187a75e1a1d6fd713f6",
+    "url": "/redux-toolkit-12/static/js/2.bc25c181.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/redux-toolkit-12/static/js/2.bc25c181.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cbbc088eaf32b3be5f85",
+    "url": "/redux-toolkit-12/static/js/main.edc70569.chunk.js"
+  },
+  {
+    "revision": "86356ae0acb4d0eace00",
+    "url": "/redux-toolkit-12/static/js/runtime-main.e4c569c7.js"
+  }
+]);
